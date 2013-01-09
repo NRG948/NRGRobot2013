78366b5a0bb2e19589c8504be58d76e36068f9f2
ForisKuang <ForisKuang@ForisKuang-PC>
2013-01-08 19:38:01 -0800

    HI ALL HI STEPHEN

---
+++ src/stephenThisIsDone/HiStephen.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stephenThisIsDone;
+
+/**
+ *
+ * @author ForisKuang
+ */
+public class HiStephen 
+{
+    
+    public void hiShank() 
+    {
+        while (true)
+        {
+            System.out.println("Hi STEPHEN SHANK!!!!!!");
+        }
+    }
+    
+}
