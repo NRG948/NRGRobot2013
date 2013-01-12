2f80d856f0907aef00069a36ef01c5af5411d66f
Hu <Hu@Hu-Laptop>
2013-01-12 14:20:39 -0800

    [no commit message]

--- src/org/usfirst/frc948/NRGRobot2013/utilities/ShooterControl.java
+++ src/org/usfirst/frc948/NRGRobot2013/utilities/ShooterControl.java
@@ -1,9 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.usfirst.frc948.NRGRobot2013.utilities;
-
 /**
  * Contains the angle and speed at which the platform fires the disc.
  * 
