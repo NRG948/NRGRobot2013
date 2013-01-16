287416369eb9420c8ad11d5db22b8fcbdf1d2223
holeung <holeung@hois-MacBook-Air.local>
2013-01-15 20:42:52 -0800

    FIX CHANGES

--- src/org/usfirst/frc948/NRGRobot2013/commands/OperatorDriveCommand.java
+++ src/org/usfirst/frc948/NRGRobot2013/commands/OperatorDriveCommand.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc948.NRGRobot2013.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc948.NRGRobot2013.OI;
 import org.usfirst.frc948.NRGRobot2013.Robot;
 import org.usfirst.frc948.NRGRobot2013.utilities.MathHelper;
 
@@ -10,42 +11,52 @@ import org.usfirst.frc948.NRGRobot2013.utilities.MathHelper;
  */
 public class OperatorDriveCommand extends Command {
 
-    private static final double SUDDEN_CHANGE_THRESHOLD = 0.8;
+    private static final double SUDDEN_CHANGE_THRESHOLD = 0.6;
+    private static final double CLOSE_TO_ZERO = 0.08;
+    private OI oi;
     private double leftMotorSpeed;
     private double rightMotorSpeed;
-
-    public OperatorDriveCommand() {
+    private double currentLeftJoystickYValue;
+    private double currentRightJoystickYValue;
+    
+    public OperatorDriveCommand(OI oi) {
+        this.oi = oi;
         requires(Robot.drive);
     }
-
+    
     protected void initialize() {
+        this.requires(Robot.drive);
         leftMotorSpeed = 0.0;
-        rightMotorSpeed = 0.0;
-    }
+        rightMotorSpeed = 0.0; 
+}
 
     protected void execute() {
         //If there is a sudden change in the y value of the joystick,
         //the average between the current motor speed and the targeted motor
-        //speed will be calculated in order to prevent damages to the motors. 
-        if (Math.abs((leftMotorSpeed - Robot.oi.getleftJoystick().getY())) >= SUDDEN_CHANGE_THRESHOLD) {
-            leftMotorSpeed = MathHelper.average(leftMotorSpeed, Robot.oi.getleftJoystick().getY());
-        } else {
-            leftMotorSpeed = Robot.oi.getleftJoystick().getY();
-        }
-
-        if (Math.abs((rightMotorSpeed - Robot.oi.getrightJoystick().getY())) >= SUDDEN_CHANGE_THRESHOLD) {
-            rightMotorSpeed = MathHelper.average(rightMotorSpeed, Robot.oi.getrightJoystick().getY());
-        } else {
-            rightMotorSpeed = Robot.oi.getrightJoystick().getY();
-        }
-        
-        //
-        if (Math.abs(leftMotorSpeed) < 0.08) {
+        //speed will be calculated in order to prevent damages to the motors.
+        currentLeftJoystickYValue = -oi.getleftJoystick().getY();
+        currentRightJoystickYValue = -oi.getrightJoystick().getY();
+        if(Math.abs(leftMotorSpeed-currentLeftJoystickYValue) < CLOSE_TO_ZERO) {
             leftMotorSpeed = 0;
         }
+        else if(Math.abs((leftMotorSpeed-currentLeftJoystickYValue))> SUDDEN_CHANGE_THRESHOLD) {
+            leftMotorSpeed = MathHelper.average(leftMotorSpeed, currentLeftJoystickYValue);
 
-        if (Math.abs(rightMotorSpeed) < 0.08) {
-            rightMotorSpeed = 0;
+        }
+        else {
+            leftMotorSpeed = currentLeftJoystickYValue;
+        }
+
+     
+        if(Math.abs(rightMotorSpeed-currentRightJoystickYValue)<CLOSE_TO_ZERO) {
+            rightMotorSpeed = 0; 
+        }
+
+        else if(Math.abs((rightMotorSpeed-currentRightJoystickYValue))> SUDDEN_CHANGE_THRESHOLD) {
+            rightMotorSpeed = MathHelper.average(rightMotorSpeed, currentRightJoystickYValue);
+        }
+        else {
+            rightMotorSpeed =currentRightJoystickYValue;
         }
 
         Robot.drive.tankDrive(leftMotorSpeed, rightMotorSpeed);
