808ac57db1eccd2dd1ee496e0715241f63cb465c
Sean Yu <aseanyu@yahoo.com>
2013-01-25 19:26:45 -0800

    Added DriveStraightdistance and drive straight time commands and deleted drive straight command. Also added drive straight method to Drive Subsystem. -sean

---
+++ src/org/usfirst/frc948/NRGRobot2013/commands/DriveStraightDistance.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc948.NRGRobot2013.commands;
+
+/**
+ *
+ * @author Sean
+ */
+public class DriveStraightDistance {
+    
+}
