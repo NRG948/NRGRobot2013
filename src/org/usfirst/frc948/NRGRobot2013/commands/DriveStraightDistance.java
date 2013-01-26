4e806ab6d4705f810161c0489375d750aedb27b5
Sean Yu <aseanyu@yahoo.com>
2013-01-26 09:42:45 -0800

    added get encoder distance method to calculate current distance according to the encoders. Added the setpoint in driveStraight method. Completed drive straight distance command. Initiated driveStraightInit method. Added static to the encoders in drive. -sean

--- src/org/usfirst/frc948/NRGRobot2013/commands/DriveStraightDistance.java
+++ src/org/usfirst/frc948/NRGRobot2013/commands/DriveStraightDistance.java
@@ -4,10 +4,40 @@
  */
 package org.usfirst.frc948.NRGRobot2013.commands;
 
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc948.NRGRobot2013.Robot;
+
 /**
  *
  * @author Sean
  */
-public class DriveStraightDistance {
+public class DriveStraightDistance extends Command{
+    double speed = 0.0;
+    double finalDistance = 0; //in feet
+    double distance = 0;
+    public DriveStraightDistance(double speed, double distance){
+        requires(Robot.drive);
+        this.speed = speed;
+        this.distance = distance;
+    }
+    protected void initialize() {
+        finalDistance = Robot.drive.getEncoderDistance() + distance;
+    }
+
+    protected void execute() {
+        Robot.drive.driveStraight(speed, Robot.drive.getDesiredHeading());
+    }
+
+    protected boolean isFinished() {
+        return (Robot.drive.getEncoderDistance() > finalDistance);
+    }
+
+    protected void end() {
+        Robot.drive.stop();
+    }
+
+    protected void interrupted() {
+        Robot.drive.stop();
+    }
     
 }
