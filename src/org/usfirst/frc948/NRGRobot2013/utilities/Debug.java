22d7810a76ab02b7e2c73430557ecad1c724776f
Kevin Kang <kevin97kang@gmail.com>
2013-01-29 21:54:38 -0800

    Debug Class...Redone and more stuff. Including rounding for printing...

--- src/org/usfirst/frc948/NRGRobot2013/utilities/Debug.java
+++ src/org/usfirst/frc948/NRGRobot2013/utilities/Debug.java
@@ -2,25 +2,97 @@ package org.usfirst.frc948.NRGRobot2013.utilities;
 
 /**
  *
- * @author Patrick
+ * @author Kevin
  */
 public class Debug {
 
-    public static final int FATAL_EXCEPTIONS = -1;
-    public static final int IO = 7;
     private static boolean print = false;
+    private static final int FATAL_EXCEPTIONS = -1;
+    public static final int ROBOT_ROUTINES = -1;
+    public static final int AUTONOMOUS = 0;
+    public static final int IR = 1;
+    public static final int DRIVE = 2;
+    public static final int JOYSTICK = 3;
+    public static final int GYRO = 4;
+    public static final int MAE = 5;
+    public static final int SHOOTER = 6;
+    public static final int IO = 7;
+    public static final int CLIMBER = 8;
+    public static final int MAE_CALIBRATION = 9;
+    public static final int TIMERS = 11;
+    private static int toggleIndex = 0;
+    private static boolean[] isEnabled = {true, true, true, true, true, true, true, true, true, true, true, true};
 
-    public static void println(String message) {
-        if (print) {
-            System.out.println(message);
-        }
+    private Debug() {
     }
-    
+
     public static void enable() {
         print = true;
     }
-    
+
     public static void disable() {
         print = false;
+    }
+
+    public static void ToggleDebug() {
+        isEnabled[toggleIndex] = !isEnabled[toggleIndex];
+    }
+
+    private static boolean isEnabled(int n) {
+        if (n < 0) {
+            return true;
+        }
+
+        return isEnabled[n];
+    }
+
+    /**
+     * prints line
+     *
+     * @param debug flag
+     * @param s string
+     */
+    public static void print(int debug, String s) {
+        if (print && isEnabled(debug)) {
+            System.out.print(s);
+        }
+    }
+    public static void println(int debug, String s) {
+        if (print && isEnabled(debug)) {
+            System.out.println(s);
+        }
+    }
+    public static void printException(Exception e) {
+        System.err.println(e.getMessage());
+        e.printStackTrace();
+    }
+
+    /**
+     *
+     * @param debug the flag
+     * @param output the array of output values
+     * @param s the message WITH number placeholder
+     * @param maxDecimalPlaces how many decimal places for the number input
+     */
+    public static void printRoundLn(int debug, String s, double[] output, int maxDecimalPlaces) {
+        if (print && isEnabled(debug)) {
+            String printString = "";
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '/') {
+                    int numberint = 0;
+                    try {
+                        numberint = Integer.parseInt(s.substring(i + 1, i + 2));
+                        double power = MathHelper.pow(10, maxDecimalPlaces);
+                        printString += ((int) (output[numberint] * power)) / power;
+                    } catch (Exception e) {
+                        printException(e);
+                    }
+                } else {
+                    printString += c;
+                }
+            }
+            System.out.println(printString);
+        }
     }
 }
