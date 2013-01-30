46fa0752a1e1f0c9ad50837397dfc2bda5637fe3
Patrick Lin <trick.rick1@live.com>
2013-01-29 19:05:07 -0800

    Added IShooter and RawShooter for the non-PID shooting command.

---
+++ src/org/usfirst/frc948/NRGRobot2013/subsystems/IShooter.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc948.NRGRobot2013.subsystems;
+
+/**
+ *
+ * @author Work
+ */
+public interface IShooter {
+    
+    public void setSpeed(double speed);
+    
+}
---
+++ src/org/usfirst/frc948/NRGRobot2013/subsystems/RawShooter.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc948.NRGRobot2013.subsystems;
+
+/**
+ *
+ * @author Work
+ */
+public class RawShooter implements IShooter {
+
+    public void setSpeed(double speed) {
+        
+    }
+    
+}
