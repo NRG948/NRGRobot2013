24673d574652003d5160d87d8df713bc13f34fac
Charles Chen <charlesyychen@gmail.com>
2013-01-31 19:38:04 -0800

    Created ClimberCommand class

---
+++ src/org/usfirst/frc948/NRGRobot2013/commands/ClimberCommand.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc948.NRGRobot2013.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ * @author Charles Patrick, Jared
+ */
+public class ClimberCommand extends Command
+{
+
+    protected void initialize() 
+    {
+    }
+
+    protected void execute() 
+    {
+    }
+
+    protected boolean isFinished() 
+    {
+        return false;
+    }
+
+    protected void end()
+    {
+    }
+
+    protected void interrupted() 
+    {
+    }
+    
+}
