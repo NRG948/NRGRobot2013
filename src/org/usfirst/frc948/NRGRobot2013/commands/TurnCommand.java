490563c6ddc9b752e448ef7fd8b2110cb8dbc8f1
irvingc <irvingc@localhost>
2013-02-02 16:27:51 -0800

    LCD output removed from TurnCommand; added constant for PID activation range

--- src/org/usfirst/frc948/NRGRobot2013/commands/TurnCommand.java
+++ src/org/usfirst/frc948/NRGRobot2013/commands/TurnCommand.java
@@ -12,25 +12,25 @@ import org.usfirst.frc948.NRGRobot2013.utilities.MathHelper;
  * @author irving
  */
 public class TurnCommand extends PIDCommand {
-    
+
     private static final double kDefaultP = 0.02;
     private static final double kDefaultI = 0.01;
     private static final double kDefaultD = 0.0;
     
+    private static final double DEGREES_CLOSE = 30.0;
     private static final double DEGREES_TOLERANCE = 2.0;
     private static final int REQUIRED_CYCLES_ON_TARGET = 20;
-
+    
     private double power;
     private double degrees;
+    
     private boolean closeToTarget;
-    
     private double pidOutput;
-    
     private int consecutiveCyclesOnTarget;
 
     public TurnCommand(double power, double degreesClockwise) {
         super(kDefaultP, kDefaultI, kDefaultD);
-        
+
         requires(Robot.drive);
 
         this.closeToTarget = false;
@@ -39,16 +39,14 @@ public class TurnCommand extends PIDCommand {
 
         this.getPIDController().setAbsoluteTolerance(DEGREES_TOLERANCE);
     }
-    
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        
-        double p = Preferences.getInstance().getDouble("TurnP", kDefaultP);
-        this.getPIDController().setPID(p, 0, 0);
-        
+        //double p = Preferences.getInstance().getDouble("InitialTurnP", kDefaultP);
+        double p = 1.0;
+        this.getPIDController().setPID(p, 0.0, 0.0);
+
         Debug.println(Debug.DRIVE, "TurnCommand intializing w/ only P: " + p);
-       
 
         Robot.drive.setDesiredHeading(Robot.drive.getDesiredHeading() + degrees);
         setSetpoint(Robot.drive.getDesiredHeading());
@@ -57,28 +55,29 @@ public class TurnCommand extends PIDCommand {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        if(!closeToTarget && Math.abs(Robot.drive.getGyroAngle()-Robot.drive.getDesiredHeading())<= 40) {
+        if (!closeToTarget && Math.abs(Robot.drive.getGyroAngle() - Robot.drive.getDesiredHeading()) <= DEGREES_CLOSE) {
             closeToTarget = true;
+            
             double p = Preferences.getInstance().getDouble("TurnP", kDefaultP);
             double i = Preferences.getInstance().getDouble("TurnI", kDefaultI);
             double d = Preferences.getInstance().getDouble("TurnD", kDefaultD);
-            
-            Debug.println(Debug.DRIVE, "TurnCommand after 40 degrees within target: " + p + " " + i + " " + d);
+
+            Debug.println(Debug.DRIVE, "TurnCommand close to target (" + DEGREES_CLOSE + " degrees)");
+            Debug.println(Debug.DRIVE, "TurnCommand adjusting PID constants: " + p + " " + i + " " + d);
         }
-        
+
         double drivePower = MathHelper.clamp(pidOutput, -power, power);
         Robot.drive.tankDrive(drivePower, -drivePower);
-        
-        
-        String setAngle = String.valueOf(MathHelper.round(getSetpoint(), 2));
-        String errorAngle = String.valueOf(MathHelper.round(this.getPIDController().getError(), 2));
-        String drivePowerStr = String.valueOf(MathHelper.round(drivePower, 2));
-        
-        LCD.clearLine(4);
-        LCD.clearLine(5);
-        LCD.println(LCD.TURNING, 4, "SET:" + setAngle + " ERR:" + errorAngle);
-        LCD.println(LCD.TURNING, 5, "  PWR:" + drivePowerStr);
-        
+
+
+//        String setAngle = String.valueOf(MathHelper.round(getSetpoint(), 2));
+//        String errorAngle = String.valueOf(MathHelper.round(this.getPIDController().getError(), 2));
+//        String drivePowerStr = String.valueOf(MathHelper.round(drivePower, 2));
+
+//        LCD.clearLine(4);
+//        LCD.clearLine(5);
+//        LCD.println(LCD.TURNING, 4, "SET:" + setAngle + " ERR:" + errorAngle);
+//        LCD.println(LCD.TURNING, 5, "  PWR:" + drivePowerStr);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -88,7 +87,7 @@ public class TurnCommand extends PIDCommand {
         } else {
             consecutiveCyclesOnTarget = 0;
         }
-        
+
         return consecutiveCyclesOnTarget >= REQUIRED_CYCLES_ON_TARGET;
     }
 
