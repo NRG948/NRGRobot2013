d670e443ed54ba8f41f4fa889e53a480b657583c
Alisha Meherally <alishasabina@aol.com>
2013-02-07 21:02:36 -0800

    Added targetDistances Utilities Class

---
+++ src/org/usfirst/frc948/NRGRobot2013/utilities/targetDistances.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc948.NRGRobot2013.utilities;
+
+/**
+ *
+ * @author ALISHA
+ */
+public class targetDistances {
+    double highTargetDistance;
+    double middleTargetDistance;
+    double lowTargetDistance;
+    boolean checkHigh;
+    boolean checkMiddle;
+    boolean checkLow;
+        
+    public void targetDistances(double highTargetDist, double middleTargetDist, double lowTargetDist, boolean checkHighT, boolean checkMiddleT, boolean checkLowT){
+        highTargetDistance=highTargetDist;
+        middleTargetDistance=middleTargetDist;
+        lowTargetDistance=lowTargetDist;
+        checkHigh=checkHighT;
+        checkMiddle=checkMiddleT;
+        checkLow=checkLowT;
+    }
+
+   
+    public double getHighDistance() {
+        return highTargetDistance;
+    }
+    public double getMiddleDistance() {
+        return middleTargetDistance;
+    }
+    public double getLowDistance(){
+        return lowTargetDistance;
+    }
+    public boolean getHighTargetFound(){
+        return checkHigh;
+    }
+    public boolean getMiddleTargetFound(){
+        return checkMiddle;
+    }
+    public boolean getLowTargetFound(){
+        return checkLow;
+    }
+    
+}
+    
+
+
