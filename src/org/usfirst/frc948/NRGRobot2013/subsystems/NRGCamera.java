6fcc900d0c9305d21e55a9fa64c6d211b71428be
Sean Yu <aseanyu@yahoo.com>
2013-02-07 19:15:34 -0800

    Added Camera Subsystem, unfinished - sean

---
+++ src/org/usfirst/frc948/NRGRobot2013/subsystems/NRGCamera.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc948.NRGRobot2013.subsystems;
+import edu.wpi.first.wpilibj.command.Subsystem;
+/**
+ *
+ * @author Sean
+ */
+public class NRGCamera extends Subsystem{
+
+    protected void initDefaultCommand() {
+    }
+    
+}
