543fc85a912458b7552b1d7fcd84ab037432c5d4
Sean Yu <aseanyu@yahoo.com>
2013-02-07 19:41:50 -0800

    Added a new servo to Robot Map, added the live actuator code for it, added change servo angle, get servo angle, get distance methods in NRGCamera, getDistance still in progress, added two fianl doubles for down angle and upright angles for servo. -sean

--- src/org/usfirst/frc948/NRGRobot2013/subsystems/NRGCamera.java
+++ src/org/usfirst/frc948/NRGRobot2013/subsystems/NRGCamera.java
@@ -4,13 +4,31 @@
  */
 package org.usfirst.frc948.NRGRobot2013.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc948.NRGRobot2013.RobotMap;
+import edu.wpi.first.wpilibj.Servo;
 /**
  *
  * @author Sean
  */
 public class NRGCamera extends Subsystem{
-
+    public static Servo servo = RobotMap.cameraServo;
+    public static double servoAngle;
+    final static double downAngle = 0d;
+    final static double uprightAngle = 30d;
     protected void initDefaultCommand() {
+    }
+    
+    public static double getDistance(){
+        return 0;
+    }
+    
+    public static void changeServoAngle(double desiredAngle){ //changes the angle of servo to given angle
+       servo.setAngle(desiredAngle);
+    }
+    
+    public static double getServoAngle(){
+        servoAngle = servo.getAngle();
+        return servoAngle; 
     }
     
 }
