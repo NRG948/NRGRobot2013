c0b9c5ed8dcaf09599b606607ce81783826c05b9
Charles Chen <charlesyychen@gmail.com>
2013-02-17 13:20:41 -0800

    created SetCameraTilt Command

---
+++ src/org/usfirst/frc948/NRGRobot2013/commands/SetCameraTilt.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc948.NRGRobot2013.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc948.NRGRobot2013.OI;
+import org.usfirst.frc948.NRGRobot2013.Robot;
+import org.usfirst.frc948.NRGRobot2013.RobotMap;
+
+/**
+ *
+ * @author Charles
+ */
+public class SetCameraTilt extends Command{
+    private double servoVal;
+    private boolean useSlider;
+    private OI oi = Robot.oi;
+    public SetCameraTilt()
+    {
+        requires(Robot.camera);
+        useSlider = true;
+    }
+    public SetCameraTilt(double servoVal)
+    {
+        servoVal = this.servoVal;
+        useSlider = false;
+    }
+    protected void initialize() {
+        
+    }
+
+    protected void execute() {
+        
+        if ( useSlider )
+        {
+            servoVal = oi.getCameraTilt();
+        }
+        RobotMap.cameraServo.set(servoVal);
+    }
+    
+    protected boolean isFinished() {
+        return !useSlider;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+        end();
+    }
+    
+}
