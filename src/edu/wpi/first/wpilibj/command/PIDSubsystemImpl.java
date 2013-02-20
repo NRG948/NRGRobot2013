9fa2df4df3046390a0ab0f626236a2c141543cf4
Charles Chen <charlesyychen@gmail.com>
2013-02-19 18:08:50 -0800

    NRGPID package and class added

---
+++ src/edu/wpi/first/wpilibj/command/PIDSubsystemImpl.java
@@ -0,0 +1,11 @@
+package edu.wpi.first.wpilibj.command;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public class PIDSubsystemImpl extends NRGPIDSubsystem {
+    
+}
