3b97456cbcf25ae2a4026b1e1548f04cd77d8296
irving <irving@turtlebook>
2013-03-21 09:17:16 -0700

    added text file logging to debug

--- src/org/usfirst/frc948/NRGRobot2013/utilities/Debug.java
+++ src/org/usfirst/frc948/NRGRobot2013/utilities/Debug.java
@@ -1,5 +1,10 @@
 package org.usfirst.frc948.NRGRobot2013.utilities;
 
+import com.sun.squawk.microedition.io.FileConnection;
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.microedition.io.Connector;
+
 /**
  *
  * @author Kevin
@@ -7,6 +12,36 @@ package org.usfirst.frc948.NRGRobot2013.utilities;
 public class Debug {
 
     private static boolean print = false;
+    
+    private static boolean loggingInitialized = false;
+    
+    private static FileConnection file = null;
+    private static OutputStream log = null;
+    
+    public static void initLogging() {
+        if (!loggingInitialized) {
+            String fileName = "Robot-log-" + System.currentTimeMillis();
+            
+            file = null;
+            log = null;
+            
+            try {
+                file = (FileConnection) Connector.open(fileName, Connector.WRITE);
+                
+                if (file != null) {
+                    log = file.openOutputStream();
+                    
+                    if (log != null) {
+                        loggingInitialized = true;
+                    } else {
+                        file.close();
+                    }
+                }
+            } catch (IOException ex) {
+                printException(ex);
+            }
+        }
+    }
     
     public static void enable() {
         print = true;
@@ -22,12 +57,29 @@ public class Debug {
     }
 
     public static void print(String s) {
+        if (loggingInitialized) {
+            try {
+                log.write(s.getBytes());
+            } catch (IOException ex) {
+                printException(ex);
+            }
+        }
+        
         if (print) {
             System.out.print(s);
         }
     }
     
     public static void println(String s) {
+        if (loggingInitialized) {
+            try {
+                log.write(s.getBytes());
+                log.write('\n');
+            } catch (IOException ex) {
+                printException(ex);
+            }
+        }
+        
         if (print) {
             System.out.println(s);
         }
