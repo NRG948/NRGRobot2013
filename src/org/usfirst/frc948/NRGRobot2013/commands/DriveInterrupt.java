d1eae79e963c83094303e1f3d6f0751edcbe27c6
irvingc <irvingc@localhost>
2013-04-26 10:43:47 -0500

    added debug print to DriveInterrupt

--- src/org/usfirst/frc948/NRGRobot2013/commands/DriveInterrupt.java
+++ src/org/usfirst/frc948/NRGRobot2013/commands/DriveInterrupt.java
@@ -2,6 +2,7 @@ package org.usfirst.frc948.NRGRobot2013.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc948.NRGRobot2013.Robot;
+import org.usfirst.frc948.NRGRobot2013.utilities.Debug;
 
 /**
  *
@@ -11,7 +12,7 @@ public class DriveInterrupt extends Command {
     
     public DriveInterrupt() { requires(Robot.drive); }
     protected void initialize() {}
-    protected void execute() {}
+    protected void execute() { Debug.println("[DriveInterrupt]"); }
     protected boolean isFinished() { return true; }
     protected void end() {}
     protected void interrupted() {}
